4110a9ee99b04798007242afd5cbd0144e096450
Mayurdhwaj <mayurdhwajs94@gmail.com>
2023-08-18 15:39:15 +0530

    changed name

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -6,7 +6,7 @@ export const Intro = () => {
       <motion.div initial={{ y: 20, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.5 }}>
         <p className="text-xl tracking-wider md:text-2xl lg:text-3xl lg:mb-1">Hi, I am</p>
         <p className="text-3xl tracking-wider md:text-4xl lg:text-5xl lg:mb-1">
-          Rakshit <strong>Arora</strong>,
+          Mayurdhwaj <strong>Singh</strong>,
         </p>
         <p className="mt-2 text-lg md:text-xl lg:text-2xl ">and I love building cool stuff 🛠️</p>
       </motion.div>
